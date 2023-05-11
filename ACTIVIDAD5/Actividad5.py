6d4548d9834a4cdd1047470895ca734578c2dffb
EMILIAN01 <88800602+EMILIAN01@users.noreply.github.com>
2023-05-11 11:42:59 -0600

    Update Actividad5.py

--- ACTIVIDAD5/Actividad5.py
+++ ACTIVIDAD5/Actividad5.py
@@ -76,7 +76,7 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 25, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 

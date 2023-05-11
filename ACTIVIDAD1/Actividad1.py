92c81cf980c9301d2aae7838f91ef8ba323677be
A01379736 <A01379736@tec.mx>
2023-05-11 11:36:19 -0600

    Update Actividad1.py

--- ACTIVIDAD1/Actividad1.py
+++ ACTIVIDAD1/Actividad1.py
@@ -8,12 +8,13 @@ Exercises
 4. Complete triangle.
 5. Add width parameter.
 """
-#
+#Importa bibliotecas
 from turtle import *
 
 from freegames import vector
 
-
+#Se define la función line que acepta dos argumentos y
+#que dibuja líneas utilizando la biblioteca turtle.
 def line(start, end):
     """Draw line from start to end."""
     up()
@@ -21,7 +22,8 @@ def line(start, end):
     down()
     goto(end.x, end.y)
 
-
+#Se define la función "square" que acepta dos argumentos y
+#que dibuja cuadrados utilizando la biblioteca turtle.
 def square(start, end):
     """Draw square from start to end."""
     up()
@@ -35,25 +37,33 @@ def square(start, end):
 
     end_fill()
 
-
+#Se define la función "circulo" que acepta dos argumentos y
+#que dibuja círuclos utilizando la biblioteca turtle.
 def circulo(start, end):
     """Draw circle from start to end."""
+    #calcula radio tomando la diferencia entre los valores inicial y final de x
+    #tomando el valor absoluto de este.
     radius = abs(end - start)
     up()
+    #se mueve el cursor a la posición para comenzar a dibujar.
     goto(start.x, start.y - radius)
     down()
     begin_fill()
+    #Llama a la función "circle" para dibujar el círculo.
     circle(radius)
+    #Cierra la figura
     end_fill()
 
+#Se define la función "rectangle" que acepta dos argumentos y
+#que dibuja rectángulos utilizando la biblioteca turtle.
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    # Ir al primer punto
+    #Ir al primer punto
     up()
     goto(start.x, start.y)
     down()
 
-    # Dibujar el rectángulo
+    #Dibujar el rectángulo
     begin_fill()
     for _ in range(2):
         forward(end.x - start.x)
@@ -62,7 +72,8 @@ def rectangle(start, end):
         left(90)
     end_fill()
 
-
+#Se define la función "triangle" que acepta dos argumentos y
+#que dibuja triángulos utilizando la biblioteca turtle.
 def triangle(start, end):
     """Draw triangle from start to end."""
     # Ir al primer punto
@@ -82,8 +93,11 @@ def triangle(start, end):
     goto(start.x, start.y)
     end_fill()
 
+#Se define la función "tap", la cuál se llama cuando
+#el usuario hace click 
 def tap(x, y):
     """Store starting point or draw shape."""
+    #Guarda el punto de inicio
     start = state['start']
 
     if start is None:
@@ -94,12 +108,12 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
-
+#Función que guarda diferentes valores en "state".
 def store(key, value):
     """Store value in state at key."""
     state[key] = value
 
-
+#Funciones que definen las acciones que llaman a otras funciones.
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
